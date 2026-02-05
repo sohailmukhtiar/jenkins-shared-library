4aeb0761cd3d2f16c98bab8ca92c2e51b12477cc
Sohail Mukhtiar <sohailmukhtiarali@gmail.com>
2026-02-05 18:13:28 +0500

    Update trivyScan.groovy

--- vars/trivyScan.groovy
+++ vars/trivyScan.groovy
@@ -2,11 +2,27 @@ def call(String imageTag) {
     sh """
     mkdir -p trivy-reports
 
+    echo "🔍 Scanning Backend Image (HTML + JSON)"
+    trivy image \
+      --severity LOW,MEDIUM,HIGH,CRITICAL \
+      --format template \
+      --template "@/var/lib/jenkins/trivy-templates/html.tpl" \
+      -o trivy-reports/backend.html \
+      sohail185/backend-app:${imageTag} || true
+
     trivy image \
       --severity LOW,MEDIUM,HIGH,CRITICAL \
       --format json \
       -o trivy-reports/backend.json \
       sohail185/backend-app:${imageTag} || true
+
+    echo "🔍 Scanning Frontend Image (HTML + JSON)"
+    trivy image \
+      --severity LOW,MEDIUM,HIGH,CRITICAL \
+      --format template \
+      --template "@/var/lib/jenkins/trivy-templates/html.tpl" \
+      -o trivy-reports/frontend.html \
+      sohail185/frontend-app:${imageTag} || true
 
     trivy image \
       --severity LOW,MEDIUM,HIGH,CRITICAL \
@@ -15,4 +31,3 @@ def call(String imageTag) {
       sohail185/frontend-app:${imageTag} || true
     """
 }
-
